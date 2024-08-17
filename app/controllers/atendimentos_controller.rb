a381511be6672f0c18a8fc706480a0b5b347fb32
Marcos Vinícius <mvinic2011@gmail.com>
2024-08-17 02:57:57 -0300

    Adicionando .env e ajuste na tela de atendimentos

--- app/controllers/atendimentos_controller.rb
+++ app/controllers/atendimentos_controller.rb
@@ -60,7 +60,7 @@ class AtendimentosController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_atendimento
-      @atendimento = Atendimento.find(params[:id])
+      @atendimento = Atendimento.includes(veiculo: :cliente).find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.

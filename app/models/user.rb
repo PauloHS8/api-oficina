5319665ca79fd07d849aff3e4c6ca0a4da41bfad
Marcos Vinícius <mvinic2011@gmail.com>
2024-08-31 16:59:25 -0300

    Atualizando a sintaxe do enum de role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   belongs_to :cliente, optional: true
-  enum role: { cliente: 0, admin: 1 }
+  enum :role, { cliente: 0, admin: 1 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
